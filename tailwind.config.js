d5fc5b87b9e70a1a416e5a4392f9397d6783cb97
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2024-03-10 12:04:14 -0600

    add custom colours to tailwind config, test to ensure tailwind and colours are working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ['./*.html'],
   theme: {
     extend: {},
+    colors: {
+      'nutmeg': 'hsl(14, 45%, 36%)',
+      'raspberry': 'hsl(332, 51%, 32%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'rose-white': 'hsl(330, 100%, 98%)',
+      'eggshell': 'hsl(30, 54%, 90%)',
+      'light-grey': 'hsl(30, 18%, 87%)',
+      'wenge': 'hsl(30, 10%, 34%)',
+      'charcoal': 'hsl(24, 5%, 18%)'
+    }
   },
   plugins: [],
 }
